c702e1c6339e09c0493f63dff9a986d24d8e2590
hyuk12 <hotman7025@naver.com>
2023-05-02 12:38:46 +0900

    project 기본 설정 완료

---
+++ korea/src/test/java/com/trip/korea/KoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.korea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
