cb258f75308dc6cc9db207e145250a54d0330d31
naocod <8162857@naver.com>
2023-05-03 10:21:59 +0900

    NY - user entity, role entity 생성

--- trip-location/src/main/java/com/korea/triplocation/entity/User.java
+++ trip-location/src/main/java/com/korea/triplocation/entity/User.java
@@ -1,4 +1,28 @@
 package com.korea.triplocation.entity;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+	private int userId;
+	private int role_id;
+	private String email;
+	private String password;
+	private String name;
+	private String phone;
+	private String address;
+	
+	private LocalDate createDate;	// 계정 생성 일자
+	private Role role;
+	
+	
+	
 }
