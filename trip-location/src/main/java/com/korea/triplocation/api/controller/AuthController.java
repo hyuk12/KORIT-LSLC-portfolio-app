b248cab4a48d3cbd0980b2d34d24a2b16abeea10
dlgustn3768 <dlgustn3768@naver.com>
2023-05-03 16:01:29 +0900

    UserMapper: mapper 중복 insert수정, yml: jwt secret 값 입력, UserService: 중복체크 controller로 이동 수정

--- trip-location/src/main/java/com/korea/triplocation/api/controller/AuthController.java
+++ trip-location/src/main/java/com/korea/triplocation/api/controller/AuthController.java
@@ -1,17 +1,17 @@
 package com.korea.triplocation.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.korea.triplocation.api.dto.request.LoginReqDto;
 import com.korea.triplocation.api.dto.request.UserReqDto;
 import com.korea.triplocation.api.dto.response.DataRespDto;
 import com.korea.triplocation.service.UserService;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +22,8 @@ public class AuthController {
 
     @PostMapping("/user")
     public ResponseEntity<?> signup(@RequestBody UserReqDto userReqDto) {
-    	System.out.println(userReqDto.getEmail() + "\t" + userReqDto.getPassword());
+//    	System.out.println(userReqDto.getEmail() + "\t" + userReqDto.getPassword());
+    	userService.checkDuplicatedByEmail(userReqDto.getEmail());
     	userService.signup(userReqDto);
         return ResponseEntity.ok(DataRespDto.ofDefault());
     }
