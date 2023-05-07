f6e7e192fa8ec5b98fc730957829d4b0b92cfd7b
hyuk12 <hotman7025@naver.com>
2023-05-07 18:40:56 +0900

    feat#41: 로그인 및 회원가입 validate check
    정규식들 추가했습니다

--- trip-location/src/main/java/com/korea/triplocation/api/dto/request/UserReqDto.java
+++ trip-location/src/main/java/com/korea/triplocation/api/dto/request/UserReqDto.java
@@ -4,14 +4,29 @@ import com.korea.triplocation.entity.User;
 import lombok.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserReqDto {
+    @Email
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8~16자리로 입력해주세요.")
     private String password;
+
+    @Pattern(regexp = "^[가-힣]{2,7}$",
+            message = "이름은 한글 2~7자리로 입력해주세요.")
     private String name;
+
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$",
+            message = "전화번호를 형식에 맞게 입력해주세요.")
     private String phone;
     private String address;
     private String profileImg;
