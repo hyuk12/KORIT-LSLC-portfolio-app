708c73f80b2d41e89f7f56736aa81503d03245c7
naocod <8162857@naver.com>
2023-05-09 11:02:55 +0900

    user / auth 분리, user rest api 설정

---
+++ trip-location/src/main/java/com/korea/triplocation/repository/AuthRepository.java
@@ -0,0 +1,14 @@
+package com.korea.triplocation.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.korea.triplocation.domain.user.entity.Authority;
+import com.korea.triplocation.domain.user.entity.User;
+
+@Mapper
+public interface AuthRepository {
+
+	public int saveUser(User user);
+	public int saveAuthority(Authority authority);
+	public User findUserByEmail(String email);
+}
