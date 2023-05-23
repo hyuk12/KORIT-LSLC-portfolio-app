2ba3f2881711e7668704eab0d9eb82573eada06e
naocod <8162857@naver.com>
2023-05-23 11:49:43 +0900

    mainpage > locationCard region 따로 저장시켜서 조회

--- trip-location/src/main/java/com/korea/triplocation/service/AdminService.java
+++ trip-location/src/main/java/com/korea/triplocation/service/AdminService.java
@@ -1,21 +1,25 @@
 package com.korea.triplocation.service;
 
-import com.korea.triplocation.api.dto.request.MainImageSaveDto;
-import com.korea.triplocation.domain.travel.entity.MainImage;
-import com.korea.triplocation.domain.travel.entity.Region;
-import com.korea.triplocation.domain.user.entity.PostsImg;
-import com.korea.triplocation.repository.AdminRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.korea.triplocation.api.dto.request.MainImageSaveDto;
+import com.korea.triplocation.api.dto.response.RegionRespDto;
+import com.korea.triplocation.domain.travel.entity.MainImage;
+import com.korea.triplocation.domain.travel.entity.Region;
+import com.korea.triplocation.repository.AdminRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -65,5 +69,29 @@ public class AdminService {
                 .tempName(tempFileName)
                 .imgSize(Long.toString(file.getSize()))
                 .build();
+    }
+    
+    private String convertFilePathToUrl(String tempName) {
+		return "http://localhost:8080/image/region/" + tempName;
+	}
+    
+    public List<RegionRespDto> getRegions() {
+    	List<Region> regions = adminRepository.getRegions();
+    	List<MainImage> mainImages = adminRepository.getMainImages();
+    	List<RegionRespDto> dtos = new ArrayList<>();
+    	
+    	String imgUrl = null;
+    	
+        for (Region region : regions) {
+        	for (MainImage image : mainImages) {
+        		if(region.getRegionId() == image.getRegionId()) {
+        			RegionRespDto dto = region.toDto();
+        			imgUrl = convertFilePathToUrl(image.getTempName());
+        			dto.setRegionImgUrl(imgUrl);
+        			dtos.add(dto);        			
+        		}
+        	}
+        }
+    	return dtos;
     }
 }
